a9d7415c0ac43ce903f95e99533388c98326eec1
Andaluzov <zhurbasi@gmail.com>
2020-10-18 23:03:12 +0300

    new version

--- 6.py
+++ 6.py
@@ -2,19 +2,36 @@ my_file = open ('input.txt')
 ll = my_file.read()
 file_out = open('output.txt', 'w')
 
-if len(ll) == 5:
-    if '-' in ll and ll[2] == '-':
-         st = [word for word in ll.split('-')]
-         first = abs(ord(st[0][0])-ord(st[1][0]))
-         second = abs(int(st[0][1])-int(st[1][1]))
+p = False
+str_abc = 'ABCDEFGH'
 
-         if (first + second) == 3:
-             file_out.write('Yes')
-         else:
-            file_out.write('No')
+if len(ll) < 5:
+    p = True
+if len(ll) > 5:
+    if len(ll) == 6 and ll[5] == '\n':
+        p = False
     else:
-        file_out.write('ERROR')
+        p = True
+if ll[2] != '-':
+    p = True
+
+if ll[0].upper() in str_abc and  ll[3].upper() in str_abc:
+    first = abs(ord(ll[0])-ord(ll[3]))
 else:
+    p = True
+
+if ll[1] in '12345678'and  ll[4] in '12345678':
+    second = abs(int(ll[1])-int(ll[4]))
+else:
+    p = True
+
+if p:
     file_out.write('ERROR')
+else:
+    if(first + second) == 3 and first != 0 and second !=0:
+        file_out.write('YES')
+    else:
+        file_out.write('NO')
+
 file_out.close()
 my_file.close()
